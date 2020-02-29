7f15c21dbd0bc6265ccac7de20f72ea52b08dd0c
uponup <1030360567@qq.com>
2020-02-29 23:34:16 +0800

    bundle extension

---
+++ Example/TMCamera/NSBundle+TMBundle.h
@@ -0,0 +1,20 @@
+//
+//  NSBundle+TMBundle.h
+//  TMCamera_Example
+//
+//  Created by 龙格 on 2020/2/29.
+//  Copyright © 2020 uponup. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSBundle (TMBundle)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/TMCamera/NSBundle+TMBundle.m
@@ -0,0 +1,16 @@
+//
+//  NSBundle+TMBundle.m
+//  TMCamera_Example
+//
+//  Created by 龙格 on 2020/2/29.
+//  Copyright © 2020 uponup. All rights reserved.
+//
+
+#import "NSBundle+TMBundle.h"
+
+#import <AppKit/AppKit.h>
+
+
+@implementation NSBundle (TMBundle)
+
+@end
